cdae57ca5a5e3bd1193d1659ce204d9b671d70e5
charliecoleman97 <charlie.a.coleman@gmail.com>
2022-02-15 22:24:46 +0000

    Making progress on data grabber
    Having issues with appending the new df with old one becuase the date is used as an index

---
+++ columns_modules.py
@@ -0,0 +1,33 @@
+def add_daily_return_to_df(df):
+    df['daily_return'] = (df['Close'] / df['Close'].shift(1)) - 1
+    return df
+
+def add_cum_return_to_df(df):
+    df['cum_return'] = (1 + df['daily_return']).cumprod()
+    return df
+
+def add_bollinger_bands(df):
+    df['middle_band'] = df['Close'].rolling(window=20).mean()
+    df['upper_band'] = df['middle_band'] + 1.96 * df['Close'].rolling(window=20).std()
+    df['lower_band'] = df['middle_band'] - 1.96 * df['Close'].rolling(window=20).std()
+    return df
+
+def add_Ichimoku(df):
+    # Conversion
+    hi_val = df['High'].rolling(window=9).max()
+    low_val = df['Low'].rolling(window=9).min()
+    df['Conversion'] = (hi_val + low_val) / 2
+
+    # Baseline
+    hi_val2 = df['High'].rolling(window=26).max()
+    low_val2 = df['Low'].rolling(window=26).min()
+    df['Baseline'] = (hi_val2 + low_val2) / 2
+
+    # Spans
+    df['SpanA'] = ((df['Conversion'] + df['Baseline']) / 2).shift(26)
+    hi_val3 = df['High'].rolling(window=52).max()
+    low_val3 = df['Low'].rolling(window=52).min()
+    df['SpanB'] = ((hi_val3 + low_val3) / 2).shift(26)
+    df['Lagging'] = df['Close'].shift(-26)
+
+    return df
