a367c6c8f49b071eb7c15696579daac87a2f19f7
charliecoleman97 <charlie.a.coleman@gmail.com>
2022-02-16 18:48:53 +0000

    finished the daily data grabber
    Still takes 30mins

--- daily_data_grabber.py
+++ daily_data_grabber.py
@@ -1,12 +1,11 @@
-from datetime import date
+from datetime import datetime, date, timedelta
 import variables
 import os
-from os import listdir
-from os.path import isfile, join
-import numpy as np
+# from os import listdir
+# from os.path import isfile, join
 import yfinance as yf
 import pandas as pd 
-import sys
+import columns_modules as cm
 
 '''
 This script will update all the stocks CSVs with daily stock data - works better than having to redownload the
@@ -15,8 +14,6 @@ CSVs again each time to update them. It will also add Span A, Span B columns as 
 # get Path to csv
 PATH = variables.stocks
 
-# Get todays date
-today = date.today()
 
 # Get Dataframes from csv
 def get_stock_df_from_csv(ticker):
@@ -32,13 +29,34 @@ def get_stock_df_from_csv(ticker):
 files = [x for x in os.listdir(PATH) if os.path.isfile(os.path.join(PATH, x))]
 tickers = [os.path.splitext(x)[0] for x in files]
 # tickers.remove('.ds_Store')
-
 tickers.sort()
-len(tickers)
 
-# We want to find the last row in the dataframe (most recent date) and store it as
-# dataframe 
-new_df = get_stock_df_from_csv(tickers[10])
-new_df.head()
 
-print("It worked")
+count = 0 
+for ticker in tickers:
+    count += 1
+    try: 
+        old_df = get_stock_df_from_csv(ticker)
+        start_date = (datetime.strptime(old_df.index[-1], "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
+        end_date = datetime.today().strftime("%Y-%m-%d")
+
+        new_df = yf.download("A", start=start_date, end=end_date)
+        new_df = new_df.drop(columns="Adj Close")
+        new_df = new_df.reset_index()
+        new_df["Date"] = new_df["Date"].apply(lambda x: x.strftime('%Y-%m-%d'))
+    
+        old_df = old_df.reset_index()       
+
+        final_df = old_df.append(new_df)
+        final_df.set_index("Date")
+
+        df = cm.add_daily_return_to_df(final_df)
+        df = cm.add_cum_return_to_df(df)
+        df = cm.add_bollinger_bands(df)
+        df = cm.add_Ichimoku(df)   
+        df.to_csv(PATH + ticker + '.csv')
+
+    except Exception as ex:
+        print(ex)
+    
+    print(f'{count}/{len(tickers)}')
